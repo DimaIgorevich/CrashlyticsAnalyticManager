8b6d950485d8ca02ad004626f32f1dc1b2b38aee
dima <dzhimasan@gmail.com>
2018-08-29 18:10:58 +0300

    Initial commit

---
+++ CrashlyticsAnalyticManager/CrashlyticsAnalyticManager/CrashlyticsAnalyticManager.h
@@ -0,0 +1,19 @@
+//
+//  CrashlyticsAnalyticManager.h
+//  CrashlyticsAnalyticManager
+//
+//  Created by dima on 29.08.2018.
+//  Copyright © 2018 QuilCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CrashlyticsAnalyticManager.
+FOUNDATION_EXPORT double CrashlyticsAnalyticManagerVersionNumber;
+
+//! Project version string for CrashlyticsAnalyticManager.
+FOUNDATION_EXPORT const unsigned char CrashlyticsAnalyticManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrashlyticsAnalyticManager/PublicHeader.h>
+
+
